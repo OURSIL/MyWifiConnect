729c491b453bf830fa398893c7ef5bebeaef5543
Aries <aries.jiang@jcinfotech.com>
2015-10-26 12:35:03 +0800

    Initial commit

---
+++ app/src/main/java/com/niqiu/contant/Global.java
@@ -0,0 +1,22 @@
+package com.niqiu.contant;
+/**
+ * 
+ * @Description: TODO(Constant) 
+ * @author Snail  (Zhanghf QQ:651555765)
+ * @date 2014-4-25 下午4:56:23 
+ * @version V1.0
+ */
+public class Global {
+	
+	public static String PASSWORD = "niqiu123456";
+	public static String HOTPOT_NAME_Head = "niqiu_";
+	
+	public static int PORT = 1207;
+	public static String SITE = "192.168.43.1";
+	
+	public static String INT_SERVER_FAIL = "INTSERVER_FAIL";
+    public static String INT_SERVER_SUCCESS = "INTSERVER_SUCCESS";
+    public static String INT_CLIENT_FAIL = "INTCLIENT_FAIL";
+    public static String INT_CLIENT_SUCCESS = "INTCLIENT_SUCCESS";
+
+}
